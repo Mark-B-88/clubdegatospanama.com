bcc1d445f7127e9668a0a2f052fda52ca68bf05a
Mark <barrantes_marco@yahoo.com>
2022-06-10 14:24:06 -0500

    added standard markup and styling
    need to seperate into multiple components

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 
+import '../styles/globals.scss'
+
 const App = ({ Component, pageProps }) => {
   return (
     <Component {...pageProps} />
