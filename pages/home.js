fd9e23a26005877d2b22106845753fc746f55156
Mark <barrantes_marco@yahoo.com>
2022-06-11 01:55:46 -0500

    fixed styling issue with questions section

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,9 @@
 import HeaderComponent from '../components/Header'
 import AboutComponent from '../components/About'
-// import QuestionsComponent from '../components/Questions'
-// import DonationsComponent from '../components/Donations'
-// import ContactComponent from '../components/Contact'
-// import FooterComponent from '../components/Footer'
+import QuestionsComponent from '../components/Questions'
+import DonationsComponent from '../components/Donations'
+import ContactComponent from '../components/Contact'
+import FooterComponent from '../components/Footer'
 
 export default function HomePage() {
     return (
@@ -11,10 +11,10 @@ export default function HomePage() {
     <div className='container'>
         <HeaderComponent />
         <AboutComponent />
-        {/* <QuestionsComponent />
+        <QuestionsComponent />
         <DonationsComponent />
         <ContactComponent />
-        <FooterComponent /> */}
+        <FooterComponent />
     </div>
     </>
     )
