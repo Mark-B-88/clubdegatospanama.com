bff7c2d1d42afecc331d22da8e579765dc2fade8
Mark <barrantes_marco@yahoo.com>
2022-06-12 01:02:28 -0500

    fixed about & footer section styling
    finished responsive styling

--- pages/home.js
+++ pages/home.js
@@ -1,20 +1,20 @@
-// import HeaderComponent from '../components/Header'
-// import AboutComponent from '../components/About'
-// import QuestionsComponent from '../components/Questions'
-// import DonationsComponent from '../components/Donations'
+import HeaderComponent from '../components/Header'
+import AboutComponent from '../components/About'
+import QuestionsComponent from '../components/Questions'
+import DonationsComponent from '../components/Donations'
 import ContactComponent from '../components/Contact'
-// import FooterComponent from '../components/Footer'
+import FooterComponent from '../components/Footer'
 
 export default function HomePage() {
     return (
     <>
     <div className='container'>
-        {/* <HeaderComponent /> */}
-        {/* <AboutComponent /> */}
-        {/* <QuestionsComponent /> */}
-        {/* <DonationsComponent /> */}
+        <HeaderComponent />
+        <AboutComponent />
+        <QuestionsComponent />
+        <DonationsComponent />
         <ContactComponent />
-        {/* <FooterComponent /> */}
+        <FooterComponent />
     </div>
     </>
     )
