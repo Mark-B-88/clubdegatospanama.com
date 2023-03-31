fb3dad168ebdff09bdf18be2c364d09084cf53c3
Marco Vinico <barrantes_marco@yahoo.com>
2023-03-30 21:29:37 -0500

    get full year function

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,13 @@
 import footerStyles from '../styles/Footer.module.scss'
 
-export default function FooterComponent(){
+export default function FooterComponent() {
+    const getFullYear = () => new Date().getFullYear()
+
     return (
         <footer className={footerStyles.footer} id='footer'>
             <div className={footerStyles.row}>
                 <div className={footerStyles.col}>
-                    <p>Copyright &copy; 2022 | <span>Club De Gatos Panama</span></p>
+                    <p>Copyright &copy; {getFullYear()} | <span>Club De Gatos Panama</span></p>
                     <img src='/images/logos/site_logo.svg' />
                 </div>
             </div>
